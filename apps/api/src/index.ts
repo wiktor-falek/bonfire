f8c965eb3944adf8940e8ffe74340f53691684ce
wiktor-falek <falekwiktor@gmail.com>
2023-09-14 17:08:47 +0200

    graceful shutdown

---
+++ apps/api/src/index.ts
@@ -0,0 +1,25 @@
+import app from "./app.js";
+import Mongo from "./mongo.js";
+
+await Mongo.connect()
+  .then(() => {
+    console.log("Database connection established");
+  })
+  .catch((reason) => {
+    console.log("Database connection failed. Reason:", reason);
+  });
+
+const server = app.listen(3000, () => {
+  console.log(`Listening on http://localhost:3000`);
+});
+
+const gracefulShutdown = async (signal: string) => {
+  console.log(`\nReceived signal: ${signal}`);
+  console.log("Shutting down gracefully...");
+  server.close();
+  await Mongo.close();
+  process.exit(0);
+};
+
+process.on("SIGINT", gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
---
+++ apps/api/src/mongo.ts
@@ -0,0 +1,42 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+const URI = "mongodb://localhost:27017";
+
+class Mongo {
+  private static client?: MongoClient;
+
+  static isInitialized() {
+    return this.client !== undefined;
+  }
+
+  static getClient(): MongoClient {
+    if (this.client === undefined) {
+      this.client = new MongoClient(URI, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        },
+      });
+    }
+    return this.client;
+  }
+
+  static async connect() {
+    try {
+      const client = await this.getClient().connect();
+      console.log(`Connected to ${process.env.NODE_ENV ?? "development"} db`);
+      return client;
+    } catch (err) {
+      console.log("Failed to connect to db");
+      throw err;
+    }
+  }
+
+  static async close(): Promise<boolean> {
+    this.client?.close();
+    return this.client !== undefined;
+  }
+}
+
+export default Mongo;
