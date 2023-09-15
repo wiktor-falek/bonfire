931e0687bc59a38b1a24b32056308c0119cf9d35
wiktor-falek <falekwiktor@gmail.com>
2023-09-15 18:54:51 +0200

    loading screen

--- apps/frontend/src/views/App.vue
+++ apps/frontend/src/views/App.vue
@@ -1,7 +1,43 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const loading = ref(true);
+
+onMounted(() => {
+  const MINIMUM_LOAD_TIME = 1000;
+  const start = Date.now();
+  // TODO: connect to socket server, watch server connection state
+  const end = Date.now();
+  const timeoutDuration = MINIMUM_LOAD_TIME - (end - start);
+
+  setTimeout(() => {
+    loading.value = false;
+  }, timeoutDuration);
+});
+</script>
+
 <template>
-  <main>
+  <div class="loading" v-if="loading">
+    <p>Loading...</p>
+  </div>
+  <main v-else>
     <h1>App View</h1>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.loading {
+  width: 100vw;
+  height: 100vh;
+  background-color: #2b2d31;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+main {
+  width: 100vw;
+  height: 100vh;
+  background-color: #313338;
+}
+</style>
