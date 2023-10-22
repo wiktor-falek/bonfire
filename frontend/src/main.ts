800a0c0fed99345f924c73704d9abd25eba35d5d
wiktor-falek <falekwiktor@gmail.com>
2023-10-23 00:06:03 +0200

    remove vue query, add missing dependency for vite build

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,11 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
-import { VueQueryPlugin } from "@tanstack/vue-query";
 import { createPinia } from "pinia";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(VueQueryPlugin);
 
 app.mount("#app");
