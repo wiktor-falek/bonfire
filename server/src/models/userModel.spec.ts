740303e68d7df34dd64f142d22b03e36a83ff0c3
wiktor-falek <falekwiktor@gmail.com>
2023-11-28 16:11:49 +0100

    set up in memory database and write first unit test for user model

--- server/src/models/userModel.spec.ts
+++ server/src/models/userModel.spec.ts
@@ -1,10 +1,56 @@
-import { expect, it, test } from "vitest";
+import { describe, it, expect, assert } from "vitest";
 import UserModel from "./userModel.js";
-import { getInMemoryMongoDb } from "src/tests/utils.js";
+import { getInMemoryMongoDb } from "../tests/utils.js";
+import { createUser } from "../entities/user.js";
 
-test("it works", () => {
-  it("ballin", async () => {
-    const inMemoryDb = await getInMemoryMongoDb();
-    const userModel = new UserModel(inMemoryDb);
+describe("user operations", async () => {
+  const inMemoryDb = await getInMemoryMongoDb();
+  const userModel = new UserModel(inMemoryDb);
+
+  it("creates a user", async () => {
+    const user = createUser({
+      username: "username",
+      displayName: "displayName",
+      email: "test@email.com",
+      hash: "whatever",
+    });
+
+    const result = await userModel.createUser(user);
+    expect(result.ok).toBe(true);
+  });
+
+  it("prevents duplicate credentials", async () => {
+    const duplicateUsernameUser = createUser({
+      username: "username",
+      displayName: "whatever",
+      email: "whatever@email.com",
+      hash: "whatever",
+    });
+
+    const duplicateEmailUser = createUser({
+      username: "whatever",
+      displayName: "whatever",
+      email: "test@email.com",
+      hash: "whatever",
+    });
+
+    const duplicateUsernameResult = await userModel.createUser(
+      duplicateUsernameUser
+    );
+    const duplicateEmailResult = await userModel.createUser(duplicateEmailUser);
+
+    expect(duplicateUsernameResult.ok).toBe(false);
+    expect(duplicateEmailResult.ok).toBe(false);
+  });
+
+  it("finds the existing user", async () => {
+    const userByUsername = await userModel.findByUsername("username");
+    assert(userByUsername !== null);
+
+    const userByEmail = await userModel.findByEmail("test@email.com");
+    assert(userByEmail !== null);
+
+    const userById = await userModel.findById(userByUsername.id);
+    assert(userById !== null);
   });
 });
--- server/src/tests/utils.ts
+++ server/src/tests/utils.ts
@@ -1,9 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
-import Mongo from "src/db/mongo.js";
+import Mongo from "../db/mongo.js";
+import createIndexes from "../helpers/createIndexes.js";
 
 export async function getInMemoryMongoDb() {
   const server = await MongoMemoryServer.create();
   const uri = server.getUri();
   const mongoClient = await new Mongo(uri).connect();
-  return mongoClient.db("bonfire");
+  const mongoDb = mongoClient.db("bonfire");
+  await createIndexes(mongoDb);
+  return mongoDb;
 }
