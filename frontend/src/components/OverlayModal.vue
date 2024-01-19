8fe8c5770d6a21d26ec24a82459cf3490203a07b
wiktor-falek <falekwiktor@gmail.com>
2024-01-19 17:07:12 +0100

    refactor modals

---
+++ frontend/src/components/OverlayModal.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+defineProps<{ isOpen: boolean }>();
+const emit = defineEmits(["close"]);
+
+function handleClose(e: Event) {
+  console.log(e.target);
+  emit("close");
+}
+</script>
+
+<template>
+  <Teleport to="body">
+    <div class="overlay-modal" @click="handleClose" v-show="isOpen">
+      <div @click.stop>
+        <slot></slot>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<style scoped>
+.overlay-modal {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 50;
+}
+</style>
---
+++ frontend/src/components/RelativeModal.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { OnClickOutside } from "@vueuse/components";
+
+defineProps<{ isOpen: boolean }>();
+const emit = defineEmits(["close"]);
+
+function handleClose() {
+  emit("close");
+}
+</script>
+
+<template>
+  <OnClickOutside @trigger="handleClose">
+    <div class="relative-modal" v-show="isOpen">
+      <slot></slot>
+    </div>
+  </OnClickOutside>
+</template>
+
+<style scoped>
+.relative-modal {
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 50;
+}
+</style>
