1d83a6e3da5dfb5884f34bfd15db0d90860d4612
wiktor-falek <falekwiktor@gmail.com>
2024-03-02 18:02:47 +0100

    wrap maps in refs

--- frontend/src/stores/userProfilesStore.ts
+++ frontend/src/stores/userProfilesStore.ts
@@ -1,32 +1,30 @@
 import { defineStore } from "pinia";
 import type { UserProfile } from "../api/users";
 import socket, { socketEmitter } from "../socket";
-import { ref, type Ref } from "vue";
+import { ref } from "vue";
 
 export const useUserProfilesStore = defineStore("userProfiles", () => {
   socketEmitter.on(
     "subscription:user-profile:status",
     ({ profileId, status }) => {
-      const profile = userProfiles.get(profileId)?.profile;
+      const profile = userProfiles.value.get(profileId)?.profile;
       if (!profile) {
         return console.error("Tried to update subscribed profile, found none");
       }
 
-      profile.value.status = status;
+      profile.status = status;
     }
   );
 
   // userId to profile data mapping
-  const userProfiles = new Map<
-    string,
-    { profile: Ref<UserProfile>; invalidated?: boolean }
-  >();
+  const userProfiles = ref(
+    new Map<string, { profile: UserProfile; invalidated?: boolean }>()
+  );
 
   // channelId to profile data mapping
-  const directMessageChannelProfiles = new Map<
-    string,
-    { profile: Ref<UserProfile>; invalidated?: boolean }
-  >();
+  const directMessageChannelProfiles = ref(
+    new Map<string, { profile: UserProfile; invalidated?: boolean }>()
+  );
 
   function _subscribeToProfilesChanges(profiles: UserProfile[]) {
     socket.send(
@@ -47,30 +45,30 @@ export const useUserProfilesStore = defineStore("userProfiles", () => {
   }
 
   function getUserProfile(userId: string) {
-    return userProfiles.get(userId)?.profile;
+    return userProfiles.value.get(userId)?.profile;
   }
 
   function setUserProfile(profile: UserProfile) {
-    userProfiles.set(profile.id, { profile: ref(profile) });
+    userProfiles.value.set(profile.id, { profile });
     _subscribeToProfilesChanges([profile]);
   }
 
   function setUserProfiles(profiles: UserProfile[]) {
     for (const profile of profiles) {
-      userProfiles.set(profile.id, { profile: ref(profile) });
+      userProfiles.value.set(profile.id, { profile });
     }
     _subscribeToProfilesChanges(profiles);
   }
 
   function getDirectMessageChannelProfile(channelId: string) {
-    return directMessageChannelProfiles.get(channelId)?.profile;
+    return directMessageChannelProfiles.value.get(channelId)?.profile;
   }
 
   function setDirectMessageChannelProfile(
     channelId: string,
     profile: UserProfile
   ) {
-    directMessageChannelProfiles.set(channelId, { profile: ref(profile) });
+    directMessageChannelProfiles.value.set(channelId, { profile });
     _subscribeToProfilesChanges([profile]);
   }
 
